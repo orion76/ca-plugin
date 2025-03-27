3953f5000fb0060ab79d5a23ba910c92eacd29be
Paul Glazkow <gpb@hos247.com>
2025-03-27 16:34:58 +0400

    work

--- src/public-api.ts
+++ src/public-api.ts
@@ -1,11 +1,4 @@
-export * from './plugin-discovery/types';
-export * from './plugin-discovery/plugin-discovery-base';
-
-export * from './plugin/types';
-export * from './plugin/plugin.base';
-
-export * from './plugin-manager/types';
-export * from './plugin-manager/plugin-manager.base';
-
-export * from './plugin-builder/types';
-export * from './plugin-builder/plugin-builder.base';
+export * from './plugin-discovery';
+export * from './plugin';
+export * from './plugin-manager';
+export * from './plugin-builder';
